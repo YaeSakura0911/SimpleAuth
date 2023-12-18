2320597e8e2f502af3c786dfd31a9f5d4e4bd60d
YaeSakura <kevin1512979396@outlook.com>
2023-12-18 23:27:21 +0800

    新增自定义记住我服务、自定义令牌存储实现类和保持登录持久层

---
+++ SimpleAuth-Framework/src/main/java/org/eu/yaesakura/simpleauth/framework/CustomRememberMeServices.java
@@ -0,0 +1,2 @@
+package org.eu.yaesakura.simpleauth.framework;public class CustomRememberMeServices {
+}
---
+++ SimpleAuth-Framework/src/main/java/org/eu/yaesakura/simpleauth/framework/mapper/PersistentLoginMapper.java
@@ -0,0 +1,2 @@
+package org.eu.yaesakura.simpleauth.framework.mapper;public interface PersistentLoginMapper {
+}
