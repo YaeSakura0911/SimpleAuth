56c7dd58680b3f88b163b975a9927d60f5a48649
YaeSakura <jiangkaiwen0911@gmail.com>
2023-12-22 17:31:38 +0800

    跑通Github OAuth2认证流程

--- SimpleAuth-Framework/src/test/java/org/eu/yaesakura/simpleauth/framework/FrameworkApplicationTest.java
+++ SimpleAuth-Framework/src/test/java/org/eu/yaesakura/simpleauth/framework/FrameworkApplicationTest.java
@@ -2,12 +2,29 @@ package org.eu.yaesakura.simpleauth.framework;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.client.ClientHttpRequestFactorySettings;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.security.core.parameters.P;
+import org.springframework.security.oauth2.core.endpoint.OAuth2AccessTokenResponse;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 
 @SpringBootTest
 public class FrameworkApplicationTest {
 
     @Test
     public void contextLoad() {
-
+        
     }
 }
